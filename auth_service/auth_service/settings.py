053327454654d9fff9182a2c41b0d82414bc6721
unknown <uwanaudofia8@gmail.com>
2025-09-02 14:14:49 +0100

    Added gunicorn and allowed hosts

--- auth_service/auth_service/settings.py
+++ auth_service/auth_service/settings.py
@@ -10,7 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'your-default-secret-key')
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    os.getenv('ALLOWED_HOST', 'localhost'),
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
